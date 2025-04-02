dc865b25ee4844375bc6155aa69dbca81c31d17f
chunk <thewoodcutter@ymail.com>
2025-04-02 21:43:00 +0000

    Updated with better working configurations!

--- srv/git/git.em.sh
+++ srv/git/git.em.sh
@@ -2,6 +2,6 @@
 cd /srv/git
 sudo chown -R $USER:$USER /srv/git
 git clone git@github.com:the8woodcutter/the8woodcutter
-git clone git@github:the8woodcutter/pyblog
-git clone git@github:the8woodcutter/mrbot
-git clone git@github:the8woodcutter/pubsubtools
+git clone git@github.com:the8woodcutter/pyblog
+git clone git@github.com:the8woodcutter/mrbot
+git clone git@github.com:the8woodcutter/pubsubtools
