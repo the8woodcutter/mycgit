606a6a892d32b162bcc32a41451926576d2a0f8c
cmdr_chunk <chunk@inspirenet.org>
2025-04-04 04:13:01 +0000

    added 3 mirror repos

--- srv/git/git.em.sh
+++ srv/git/git.em.sh
@@ -6,3 +6,6 @@ git clone git@github.com:the8woodcutter/pyblog
 git clone git@github.com:the8woodcutter/mrbot
 git clone git@github.com:the8woodcutter/pubsubtools
 git clone git@github.com:the8woodcutter/mycgit
+git clone https://github.com/PurpleI2P/i2pd-tools.git
+git clone https://gt.kalli.st/czar/mod_deepweb.git
+git clone https://github.com/jerry-harm/mod_i2p
